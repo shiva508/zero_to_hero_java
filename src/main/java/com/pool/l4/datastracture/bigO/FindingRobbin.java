9767aebab11465f0e8a87c1c2e884df0eb2809d0
shiva508 <dasarishiva1@gmail.com>
2022-04-08 08:01:08 +0530

    big o types

--- src/main/java/com/pool/l4/datastracture/bigO/FindingRobbin.java
+++ src/main/java/com/pool/l4/datastracture/bigO/FindingRobbin.java
@@ -1,15 +1,17 @@
 package com.pool.l4.datastracture.bigO;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class FindingRobbin {
 	public static void main(String[] args) {
-		String[] batmanAssociates1 = new String[1000];
-		Arrays.fill(batmanAssociates1, "SHIVA");
-		robbinSearch();
-		bigOone(batmanAssociates1);
-		bigOoneChallenge(batmanAssociates1);
-
+		// String[] batmanAssociates1 = new String[1000];
+		// Arrays.fill(batmanAssociates1, "SHIVA");
+		// robbinSearch();
+		// bigOone(batmanAssociates1);
+		// bigOoneChallenge(batmanAssociates1);
+		// logAllPairOfNumbers();
+		dropNonDoominant();
 	}
 
 	private static void anotherFunChallenge(String[] input) {
@@ -46,7 +48,7 @@ public class FindingRobbin {
 	}
 
 	private static void bigOone(String[] batmanAssociates1) {
-		System.out.println(batmanAssociates1[0]);
+		System.out.println(batmanAssociates1[0]);// O(1)
 
 	}
 
@@ -69,4 +71,63 @@ public class FindingRobbin {
 
 		System.out.println(endTime - startTime);
 	}
+
+	// REMOVING THE CONSTANTS
+	private static void bigOoneRule2(String[] batmanAssociates1) {
+		System.out.println(batmanAssociates1[0]);// O(1)
+		var middleIndex = Math.floor(batmanAssociates1.length / 2);// O(1)
+		int index = 0;// O(1)
+		while (index < middleIndex) {// O(n/2)
+			System.out.println(batmanAssociates1[index]);
+			index++;
+		}
+		for (int i = 0; i < 100; i++) {// 100
+			System.out.println("Hey there");
+		}
+		// O(1+n/2+100)==>O(n)=====>after removing constants
+	}
+
+	// DIFFERENT INPUTS
+	public void differntInputs(List<String> input1, List<String> input2) {
+		input1.forEach(data -> {// O(a)
+			System.out.println(data);
+		});
+		input2.forEach(data -> {// O(b)
+			System.out.println(data);
+		});
+
+		// O(a+b)
+	}
+
+	// LOG ALL PAIR OF NUMBERS
+
+	public static void logAllPairOfNumbers() {
+		int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		for (int i = 0; i < input.length; i++) { // O(n)
+			for (int j = 0; j < input.length; j++) {// O(n)
+				System.out.println("I={" + input[i] + "," + input[j] + "}");
+			}
+		}
+
+		// O(n*n)===>O(n^2)
+	}
+
+	// RULE4:Drop the non dominant
+
+	public static void dropNonDoominant() {
+		int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		for (int i = 0; i < input.length; i++) {//O(n)
+			System.out.println(input[i]);
+		}
+
+		for (int i = 0; i < input.length; i++) { // O(n)
+			for (int j = 0; j < input.length; j++) {// O(n)
+				System.out.println("SUM={" + input[i] + input[j] + "}");
+			}
+		}
+
+		// O(n+ n*n)===>O(n+ n^2)=====>
+	}
+
 }
