d34f3e7c670135886e5cad70a0af0dc12c4963c8
shiva508 <dasarishiva1@gmail.com>
2022-04-12 08:17:40 +0530

    Queue

--- src/main/java/com/pool/l1/util/queue/QueueSimullator.java
+++ src/main/java/com/pool/l1/util/queue/QueueSimullator.java
@@ -1,14 +1,49 @@
 package com.pool.l1.util.queue;
 
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class QueueSimullator {
 	public static void main(String[] args) {
 		queueBasicOperation();
+		queueBasicOperationPriorityQueue();
 	}
 
 	private static void queueBasicOperation() {
-		Queue<String> queue=new LinkedList<>();
+		Queue<String> queue = new LinkedList<>();
+		queue.add("A");
+		queue.add("B");
+		queue.add("C");
+		queue.add("D");
+		queue.add("E");
+		queue.offer("F");
+		System.out.println(queue);
+		// REMOVE THE QUEUE
+		queue.remove("C");
+		System.out.println(queue);
+		System.out.println("===========================================");
+	}
+
+	private static void queueBasicOperationPriorityQueue() {
+
+		Queue<String> priorityQueue = new PriorityQueue<>();
+		priorityQueue.add("A");
+		priorityQueue.add("B");
+		priorityQueue.add("C");
+		priorityQueue.add("D");
+		priorityQueue.add("E");
+		priorityQueue.offer("F");
+
+		String peek = priorityQueue.peek();
+		System.out.println("peek:" + peek);
+
+		String pool = priorityQueue.poll();
+		System.out.println("pool:" + pool);
+
+		System.out.println(priorityQueue);
+		// REMOVE THE QUEUE
+		priorityQueue.remove("C");
+		System.out.println(priorityQueue);
 	}
 }
---
+++ src/main/java/com/pool/l1/util/queue/blockingqueue/Topic.java
@@ -0,0 +1,19 @@
+package com.pool.l1.util.queue.blockingqueue;
+
+public class Topic {
+	private String topicName;
+
+	public Topic() {
+
+	}
+
+	public String getTopicName() {
+		return topicName;
+	}
+
+	public Topic setTopicName(String topicName) {
+		this.topicName = topicName;
+		return this;
+	}
+
+}
