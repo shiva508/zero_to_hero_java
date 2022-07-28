e46e7d05340ffe7c78cd7910d4318d030a5825fc
shiva508 <dasarishiva1@gmail.com>
2022-07-28 21:54:29 +0530

    Polymorpisam

---
+++ src/main/java/com/pool/l0/core/oops/polymorpisam/BluetoothHeadset.java
@@ -0,0 +1,22 @@
+package com.pool.l0.core.oops.polymorpisam;
+
+public class BluetoothHeadset extends HeadSet {
+
+	@Override
+	public void playVideo() {
+		System.out.println("Playing HD vidio using Bluetooth  headset");	
+	}
+
+	public void playVideo(String datat) {
+		System.out.println("Playing HD vidio using Bluetooth  headset");	
+	}
+
+	public void playVideo(String datat,int num) {
+		System.out.println("Playing HD vidio using Bluetooth  headset");	
+	}
+
+	public static void staticCall() {
+		System.out.println(" BluetoothHeadset static test");
+	}
+
+}
